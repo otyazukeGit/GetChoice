4fda1eb548f93df6b09553d0ea0e82adde627158
uuser <otyazuke.gookle@gmail.com>
2022-01-04 21:53:01 +0900

    wip react-native-animatable

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Animatable from 'react-native-animatable';
+// import { useState } from 'react/cjs/react.production.min';
+import { useState } from 'react/cjs/react.development';
 
 export default function App() {
+const [appear, setAppear] = useState(false)
+
+const startGetChoice = () => {
+  console.log('startGetChoice');
+  setAppear(true)
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!!</Text>
       <StatusBar style="auto" />
-      <Button title="Click!" onPress={() => console.log("button")} />
+      <Button title="Click!" onPress={() => startGetChoice()} />
+      <View style={appear == true ? styles.countDownArea : styles.notAppear}>
+        <Animatable.Text
+          animation="zoomIn"
+          style={styles.countDownItem}
+          iterationCount={2}
+          onAnimationEnd={() => alert("aaaa")}
+        >
+          The choice is...
+        </Animatable.Text>
+      </View>
     </View>
   );
 }
@@ -18,4 +38,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  countDownArea: {
+    position: "absolute",
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: "30%",
+    width: "50%",
+    backgroundColor: 'skyblue',
+  },
+  countDownItem: {
+    fontSize: 20
+  },
+  notAppear: {
+    display:"none"
+  }
 });
