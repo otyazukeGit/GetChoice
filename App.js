6a12d396e0fe3770b51a49bda35140947a3ac20d
uuser <otyazuke.gookle@gmail.com>
2022-01-11 23:07:35 +0900

    ADD one zoomIn animation when onClick

--- App.js
+++ App.js
@@ -1,32 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 // import { useState } from 'react/cjs/react.production.min';
 import { useState } from 'react/cjs/react.development';
 
 export default function App() {
-const [appear, setAppear] = useState(false)
+  const [appear, setAppear] = useState(false)
 
-const startGetChoice = () => {
-  console.log('startGetChoice');
-  setAppear(true)
-}
+  const startGetChoice = () => {
+    console.log('startGetChoice');
+    setAppear(true)
+  }
+  const EndGetChoice = () => {
+    console.log('EndGetChoice');
+    setAppear(false)
+  }
+
+  const allert = () => {
+    Alert.alert("aaaa")
+  }
 
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!!</Text>
       <StatusBar style="auto" />
       <Button title="Click!" onPress={() => startGetChoice()} />
-      <View style={appear == true ? styles.countDownArea : styles.notAppear}>
-        <Animatable.Text
-          animation="zoomIn"
-          style={styles.countDownItem}
-          iterationCount={2}
-          onAnimationEnd={() => alert("aaaa")}
-        >
-          The choice is...
-        </Animatable.Text>
-      </View>
+      {/* <Button title="Click!" onPress={allert} /> */}
+      {/* <View style={appear == true ? styles.countDownArea : styles.notAppear}> */}
+      {/* <View style={appear == true ? styles.notAppear : styles.countDownArea}> */}
+      {appear == true ?
+        <View style={styles.countDownArea}>
+          <Animatable.Text
+            animation="zoomIn"
+            style={styles.countDownItem}
+            iterationCount={1}
+            onAnimationEnd={() => EndGetChoice()}
+          >
+            The choice is...
+            <Text>{appear == true ? "true" : "false"}</Text>
+          </Animatable.Text>
+        </View>
+        :
+        <View></View>
+      }
     </View>
   );
 }
@@ -43,8 +59,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    height: "30%",
-    width: "50%",
+    height: 300,
+    width: 300,
+    borderRadius: 300/2,
     backgroundColor: 'skyblue',
   },
   countDownItem: {
