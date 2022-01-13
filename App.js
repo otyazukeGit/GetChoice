7971f1448015e39deee098bee7f0e5a08f9f3129
uuser <otyazuke.gookle@gmail.com>
2022-01-13 23:08:20 +0900

    replace useState with useAtom of jotai

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 // import { useState } from 'react/cjs/react.production.min';
-import { useState } from 'react/cjs/react.development';
+// import { useState } from 'react/cjs/react.development';
 import CountDown from './src/components/CountDown';
+import { atom, useAtom, Provider } from 'jotai'
+
+/* atom() should be out of components */
+const appearAtom = atom(false)
+const appearToggleAtom = atom(
+  (get) => get(appearAtom),
+  (get, set) => {
+    set(appearAtom, true)
+  },
+)
 
 export default function App() {
-  const [appear, setAppear] = useState(false)
+  const [appear, setAppear] = useAtom(appearToggleAtom)
 
   const startGetChoice = () => {
     console.log('startGetChoice');
-    setAppear(true)
+    setAppear()
   }
-  const EndGetChoice = () => {
-    console.log('EndGetChoice');
-    setAppear(false)
-  }
+  // const EndGetChoice = () => {
+  //   console.log('EndGetChoice');
+  //   setAppear(false)
+  // }
 
   // const allert = () => {
   //   Alert.alert("aaaa")
@@ -24,12 +34,15 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!!</Text>
       <StatusBar style="auto" />
-      <Button title="Click!" onPress={() => startGetChoice()} />
-      {appear == true ?
-        <CountDown/>
-        :
-        <View></View>
-      }
+      <Provider>
+        <Text>{appear == false ? "1" : "2"}</Text>
+        <Button title="Click!" onPress={() => startGetChoice()} />
+        {appear == true ?
+          <CountDown/>
+          :
+          <View></View>
+        }
+      </Provider>
     </View>
   );
 }
